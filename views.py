6b71e4f86af5ca16955bbcc1466c6821ea6e32a6
shivamnehe96 <97394229+shivamnehe96@users.noreply.github.com>
2022-11-29 09:02:34 +0530

    Add files via upload

--- views.py
+++ views.py
@@ -1,10 +1,3 @@
 from django.shortcuts import render
-from django.http import HttpsResponse
 
-#create your views here
-
-def index(request):
-    return HttpsResponse("<h1>Welcome</h1>")
-
-def about(request):
-    return HttpsResponse("<h1>About page</h1>")
+# Create your views here.
